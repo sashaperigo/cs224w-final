e1de97c863438cff3778024e7e6c9bac62507c41
Varun Gupta <vgupta16@stanford.edu>
2017-12-03 15:32:52 -0800

    add ER sampler

--- networks.py
+++ networks.py
@@ -1,23 +1,24 @@
 from util import evolve_type
+import numpy as np
 
-# def watts_strogatz(dataframe, types_to_indices):
-#     for cell_type in ["PS", "NP", "HF"]:
-#         indices = types_of_indices[cell_type]
+# do caching where necessary
 
+def watts_strogatz_sampler(dataframe, source_index, sink_indices_list, alpha=2):
+    pass
 
-def erdos_renyi(dataframe, types_to_indices, percent=20):
-    return True
-
+def erdos_renyi_sampler(dataframe, source_index, sink_indices_list, percent=0.2):
+    assert (percent <= 1) and (percent >= 0)
+    return set(np.random.choice(sink_indices_list, int(percent * len(sink_indices_list))))
 
 # primitive cannot stay, must evolve
-def build_graph(dataframe, types_to_indices, sampler_fn):
+def build_graph(dataframe, types_to_indices, sampler_fn, sampler_fn_kwargs):
     edges = defaultdict(set)
     for primitive_type in PRIMITIVE_TYPES:
         successor_type = evolve_type(primitive_type)
         primitive_indices = types_to_indices[primitive_type]
-        successor_indices = types_to_indices[successor_type]
+        successor_indices_list = list(types_to_indices[successor_type])
         for primitive_source in primitive_indices:
-            successor_sinks = sampler_fn(dataframe, primitive_source, successor_indices)
-            for successor_sink in successor_sinks:
-                edges[primitive_source].add(successor_sink)
+            successor_sinks = sampler_fn(dataframe, primitive_source, successor_indices_list, **sampler_fn_kwargs)
+            edges[primitive_source] = successor_sinks
+
     return edges
