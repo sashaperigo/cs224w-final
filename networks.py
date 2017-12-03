b69247916711aa67e09f0bcb7d179135ffa8cfc2
Varun Gupta <vgupta16@stanford.edu>
2017-12-03 15:49:14 -0800

    add watts strogatz sampler

--- networks.py
+++ networks.py
@@ -1,16 +1,28 @@
-from util import evolve_type
+from util import evolve_type, get_weighted_euclidean_distance, get_spread_weight_series
 import numpy as np
 
 # do caching where necessary
 
-def watts_strogatz_sampler(dataframe, source_index, sink_indices_list, alpha=2):
-    pass
+def watts_strogatz_sampler(dataframe, source_index, sink_indices_list, spread_weights, alpha=2, percent=0.2):
+    proportions = np.array([
+        1/np.power(get_weighted_euclidean_distance(dataframe, source_index, sink_index, spread_weights), alpha) \
+            for sink_index in sink_indices
+    ])
+    sampled = set(np.random.choice(
+        sink_indices_list,
+        int(percent * len(sink_indices_list)),
+        p=proportions/np.sum(proportions)
+    ))
+    assert len(sampled) > 1
+    return sampled
 
 def erdos_renyi_sampler(dataframe, source_index, sink_indices_list, percent=0.2):
     assert (percent <= 1) and (percent >= 0)
-    return set(np.random.choice(sink_indices_list, int(percent * len(sink_indices_list))))
+    sampled = set(np.random.choice(sink_indices_list, int(percent * len(sink_indices_list))))
+    assert len(sampled) > 1
+    return sampled
 
-# primitive cannot stay, must evolve
+# primitive cannot stay, must evolve; easier computation in that we don't have to do unions, don't have to worry about self-loops
 def build_graph(dataframe, types_to_indices, sampler_fn, sampler_fn_kwargs):
     edges = defaultdict(set)
     for primitive_type in PRIMITIVE_TYPES:
