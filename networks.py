421210850916b013163bc143914c98cede761d75
Varun Gupta <vgupta16@stanford.edu>
2017-12-03 14:26:23 -0800

    add generalized edge creation code

--- networks.py
+++ networks.py
@@ -1,3 +1,4 @@
+from util import evolve_type
 
 # def watts_strogatz(dataframe, types_to_indices):
 #     for cell_type in ["PS", "NP", "HF"]:
@@ -8,16 +9,15 @@ def erdos_renyi(dataframe, types_to_indices, percent=20):
     return True
 
 
-def build_graph(dataframe, types_of_indices, sampler_fn):
+# primitive cannot stay, must evolve
+def build_graph(dataframe, types_to_indices, sampler_fn):
     edges = defaultdict(set)
-    for cell_type in PRIMITIVE_TYPES:
-        indices = types_to_indices[cell_type]
-        for src in indices:
-
-            for dst in indices:
-                # No self edges!
-                if src == dst:
-                    continue
-                if sampler_fn(dataframe, src, dst):
-                    # Conditional depending on what type of graph we're doing
-                    edges[src].add(dst)
+    for primitive_type in PRIMITIVE_TYPES:
+        successor_type = evolve_type(primitive_type)
+        primitive_indices = types_to_indices[primitive_type]
+        successor_indices = types_to_indices[successor_type]
+        for primitive_source in primitive_indices:
+            successor_sinks = sampler_fn(dataframe, primitive_source, successor_indices)
+            for successor_sink in successor_sinks:
+                edges[primitive_source].add(successor_sink)
+    return edges
