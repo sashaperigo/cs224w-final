d5524404d2c230a5b434161015708b2e3a299f13
Varun Gupta <vgupta16@stanford.edu>
2017-12-03 08:47:00 -0800

    move constants to defs file

---
+++ defs.py
@@ -0,0 +1,17 @@
+EVOLUTIONARY_CHAIN = {
+    'PS': ['PS', 'NP'],
+    'NP': ['NP', 'HF'],
+    'HF': ['4G', '4GF'],
+}
+
+CELL_TYPES = [
+    'PS', 'NP', 'HF', '4G', '4GF',
+]
+
+PRIMITIVE_TYPES = [
+    'PS', 'NP', 'HF',
+]
+
+TERMINAL_TYPES = [
+    '4G', '4GF',
+]
--- util.py
+++ util.py
@@ -2,24 +2,7 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 import random
-
-EVOLUTIONARY_CHAIN = {
-    'PS': ['PS', 'NP'],
-    'NP': ['NP', 'HF'],
-    'HF': ['4G', '4GF'],
-}
-
-CELL_TYPES = [
-    'PS', 'NP', 'HF', '4G', '4GF',
-]
-
-PRIMITIVE_TYPES = [
-    'PS', 'NP', 'HF',
-]
-
-TERMINAL_TYPES = [
-    '4G', '4GF',
-]
+from defs import CELL_TYPES, PRIMITIVE_TYPES, EVOLUTIONARY_CHAIN
 
 
 def get_normalized_data(measurements_path, types_path):
