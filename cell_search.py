01d1b3080ca79233631c29ce85d00d09ee1ed6fd
Varun Gupta <vgupta16@stanford.edu>
2017-12-03 17:17:29 -0800

    modify CellSearch problem to use new graph structure and defs

--- cell_search.py
+++ cell_search.py
@@ -5,35 +5,35 @@ import math
 import ingest_data
 import random
 
+from defs import TERMINAL_MARKER
+
 ############################################################
 # Abstract interfaces for search problems and search algorithms.
 
 # State-based Models for Cellular Differentiation
-
-# 
 class CellSearch:
-    def __init__(self, data, genes, transition_probabilities, start_state):
-        self.data = data
-        self.genes = genes
-        self.transition_probabilities = transition_probabilities
-        self.start_state = start_state
+    def __init__(self, start_index, edges_dict, cost_fn):
+        self.start_index = start_index
+        self.edges_dict = edges_dict
+        self.cost_fn = cost_fn
 
     # Return the start state.
     def startState(self): 
-        return self.start_state
+        return self.start_index
 
     # Return whether |state| is an end state or not.
     def isEnd(self, state):
-        return state[1] == 'FATE'
+        return state == TERMINAL_MARKER
 
     # Return a list of (action, newState, cost) tuples corresponding to edges
     # coming out of |state|.
+    # For us, action and newState are the same --> we just use the cell/node/state dataframe index.
+    # Cost is the weighted Euclidean distance between the current cell and the successor. 
     def succAndCost(self, state):
-        gene_profile, cell_type = state
         results = []
 
-        transition_probabilities = self.transition_probabilities[state]
-        for next_state, transition_probability in transition_probabilities.iteritems():
-            results.append((next_state, next_state, 1/(transition_probability)))
+        for successor_state in self.edges_dict[state]:
+            cost = self.cost_fn(sorted(state, successor_state))
+            results.append((successor_state, successor_state, cost))
         
         return results
