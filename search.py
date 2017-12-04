1acfe4915855dd7bafc3b70d0b78544699504d0e
Varun Gupta <vgupta16@stanford.edu>
2017-12-03 19:18:46 -0800

    add some shortest path code

--- search.py
+++ search.py
@@ -1,26 +1,20 @@
-from cell_search import CellSearch
-from search_util import UniformCostSearch
-import csv
-import ingest_data
-import collections
-import json
+from cell_search_problem import CellSearchProblem
+from defs import PRIMITIVE_TYPES
 
-cells_file = 'norm.csv'
-types_file = 'celltypes.txt'
-genes_file = 'genes.txt'
+def find_shortest_paths(edges_dict, types_to_indices, cost_fn):
+    actions = {}
+    for primitive_type in PRIMITIVE_TYPES:
+        for primitive_start_index in types_to_indices[primitive_type]:
+            actions[primitive_start_index] = find_shortest_path_from(
+                primitive_start_index,
+                edges_dict,
+                cost_fn,
+            )
+    return actions
 
-data, genes, transitions = ingest_data.ingest(cells_file, types_file, genes_file)
-
-num_states_explored = collections.defaultdict(int)
-total_cost = collections.defaultdict(float)
-actions = collections.defaultdict(list)
-
-for cell_type in data:
-    if cell_type != '4G' and cell_type != '4GF':
-        for starting_state in data[cell_type]:
-            cs = CellSearch(data, genes, transitions, starting_state)
-            ucs = UniformCostSearch(verbose = 1)
-            ucs.solve(cs)
-            num_states_explored[starting_state] = ucs.numStatesExplored
-            total_cost[starting_state] = ucs.totalCost
-            actions[starting_state] = ucs.actions
+def find_shortest_path_from(start_index, edges_dict, cost_fn):
+    cs = CellSearchProblem(start_index, edges_dict, cost_fn)
+    ucs = UniformCostSearch(verbose = 1)
+    ucs.solve(cs)
+    print(ucs.actions)
+    return ucs.actions
