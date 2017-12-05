81d153d0e391788e75c537dc1edc12dcf757de76
Varun Gupta <vgupta16@stanford.edu>
2017-12-04 22:18:11 -0800

    remove unnecessary dataframe mentions; fix import

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,4 @@
-from cell_search_util import find_shortest_paths
+from cell_search import find_shortest_paths
 from costs import euclidean_cost
 from defs import MEASUREMENTS_PATH, TYPES_PATH
 from networks import build_graph, complete_sampler
@@ -7,7 +7,7 @@ from util import get_normalized_data, get_cell_types_to_indices
 
 data = get_normalized_data(MEASUREMENTS_PATH, TYPES_PATH)
 cell_types_to_indices = get_cell_types_to_indices(data)
-example = build_graph(data, cell_types_to_indices, complete_sampler, {})
+example = build_graph(cell_types_to_indices, complete_sampler, {})
 
 
 find_shortest_paths(example, cell_types_to_indices, euclidean_cost)
