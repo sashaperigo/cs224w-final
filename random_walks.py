bfed5eb9183681c2d4896d721aef08610ce2bcd5
Sasha Perigo <sasha.perigo@gmail.com>
2017-12-06 18:00:41 -0800

    Remove comment

--- random_walks.py
+++ random_walks.py
@@ -1,21 +1,6 @@
 from collections import defaultdict
 from defs import PRIMITIVE_TYPES, EVOLUTIONARY_CHAIN, TERMINAL_MARKER
 
-# -for every primitive type:
-# for every primitive node of that type:
-# while primitive != TERMINAL_MARKER:
-# select the successor type
-# sample one successor node of that type according to some distribution (uniform if Erdos Renyi, distance-based if Watts-Strogatz)
-# record that successor node (or action)
-# primitive = successor
-# -return our actions
-
-# The samplers that we have currently take in percentages - we just need to either identify a percentage that returns a single node or change the percentage parameter to something else
-
-# the percentage you can use to get exactly one node back is:
-
-# float(1)/len(successor_indices_list)
-
 
 def random_walk(cell_types_to_indices, indices_to_cell_types, sampler_fn):
     indices_to_actions = defaultdict(list)
